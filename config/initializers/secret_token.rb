0fdc5da3a6440e7a40bb05cd52f9d9bf2c156f44
Robert Trotter <trotter.robert@gmail.com>
2012-05-06 12:58:11 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blacksquare::Application.config.secret_token = '78b2c62c37560118edaf193951c9b5af83cc8c343b771586ee4ae261260b38e2e8d7798c46f427c3f5ccb59e6e3d2a02802362dd02a579b3d462bbbbdafefbfe'
